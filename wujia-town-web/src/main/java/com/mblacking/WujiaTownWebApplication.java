7155a7e63cc0bf491dda0cac5d5c9cc2c00df235
H005604 <nieqingming@wtoip.com>
2019-03-15 17:06:25 +0800

    init project

---
+++ wujia-town-web/src/main/java/com/mblacking/WujiaTownWebApplication.java
@@ -0,0 +1,13 @@
+package com.mblacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WujiaTownWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WujiaTownWebApplication.class, args);
+	}
+
+}
